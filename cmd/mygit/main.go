7cffa8cedccd298f30d4fa53b6f8e62825baca21
taxintt <nthouse14@gmail.com>
2023-04-02 15:44:29 +0900

    read blob object and output to os.stdout

--- cmd/mygit/main.go
+++ cmd/mygit/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bytes"
+	"compress/zlib"
 	"fmt"
+	"io"
 	"os"
 	// Uncomment this block to pass the first stage!
 	// "os"
@@ -31,6 +34,27 @@ func main() {
 		}
 
 		fmt.Println("Initialized git directory")
+
+	case "cat-file":
+		if len(os.Args) < 4 {
+			fmt.Fprintf(os.Stderr, "specify the sha of blob object: cat-file -p <blob_sha>\n")
+			os.Exit(1)
+		}
+
+		fullSha := os.Args[3]
+		content, err := os.ReadFile(fmt.Sprintf(".git/objects/%s/%s", fullSha[:2], fullSha[2:]))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error reading blob object (binary data): %s\n", err)
+		}
+
+		buffer := bytes.NewBuffer(content)
+		r, err := zlib.NewReader(buffer)
+		defer r.Close()
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error decompressing blob object (binary data): %s\n", err)
+		}
+		io.Copy(os.Stdout, r)
 
 	default:
 		fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
