642368fba707a46c57f1c87d68bb8fd1f56022fe
taxintt <nthouse14@gmail.com>
2023-06-12 22:34:01 +0900

    fix

--- cmd/mygit/cmd.go
+++ cmd/mygit/cmd.go
@@ -211,7 +211,6 @@ func WriteTreeObject(dir string) (sha [20]byte, _ error) {
 	// create plain git tree object
 	var treeBuffer bytes.Buffer
 	for _, entry := range result {
-		log.Printf(fmt.Sprintf("%s%s %s\x00%s", entry["type"], entry["permission"], entry["name"], entry["hash"]))
 		treeBuffer.WriteString(fmt.Sprintf("%s%s %s\x00%s", entry["type"], entry["permission"], entry["name"], entry["hash"]))
 	}
 	header := fmt.Sprintf("tree %d\x00", treeBuffer.Len())
